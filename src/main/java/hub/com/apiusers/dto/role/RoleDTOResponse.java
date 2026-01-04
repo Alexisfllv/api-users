ede40d4f004e4b0c85d5986762f7126d01400de8
AlexisFl <159475523+Alexisfllv@users.noreply.github.com>
2026-01-04 09:49:06 -0500

    feat(role): add validation dto(request,response) and Swagger Schema(request,response)

--- src/main/java/hub/com/apiusers/dto/role/RoleDTOResponse.java
+++ src/main/java/hub/com/apiusers/dto/role/RoleDTOResponse.java
@@ -1,7 +1,15 @@
 package hub.com.apiusers.dto.role;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 public record RoleDTOResponse(
+
+        @Schema(description = "Unique role identifier.", example = "1")
         Long id,
+
+        @Schema(description = "Role name.", example = "Admin")
         String name,
+
+        @Schema(description = "Role description.", example = "Supervision complete and Authorizated")
         String description
 ) {}
