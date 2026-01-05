518ff10885c503cf618fcde0626215c7b805731d
AlexisFl <159475523+Alexisfllv@users.noreply.github.com>
2026-01-04 23:35:11 -0500

    feat(role): add base entity (createAt,updateAt) , role entity

---
+++ src/main/java/hub/com/apiusers/entity/Role.java
@@ -0,0 +1,28 @@
+package hub.com.apiusers.entity;
+
+import hub.com.apiusers.config.auditing.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "roles")
+public class Role extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 50)
+    private String name;
+
+    @Column(nullable = false, length = 200)
+    private String description;
+
+}
