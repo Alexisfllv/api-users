1df3d6de7ffeea11c4e8267e1ee5529277025f73
AlexisFl <159475523+Alexisfllv@users.noreply.github.com>
2026-01-08 18:11:06 -0500

    feat(test): roleServiceImplTest add test method Get pagelistRoles Test

--- src/test/java/hub/com/apiusers/service/impl/RoleServiceImplTest.java
+++ src/test/java/hub/com/apiusers/service/impl/RoleServiceImplTest.java
@@ -4,6 +4,7 @@ import hub.com.apiusers.dto.role.RoleDTOResponse;
 import hub.com.apiusers.entity.Role;
 import hub.com.apiusers.mapper.RoleMapper;
 import hub.com.apiusers.service.domain.RoleServiceDomain;
+import hub.com.apiusers.util.page.PageResponse;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
@@ -11,6 +12,13 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.*;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+
+import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -65,6 +73,44 @@ public class RoleServiceImplTest {
             InOrder inOrder = Mockito.inOrder(roleMapper,roleServiceDomain);
             inOrder.verify(roleServiceDomain).roleExists(roleIdExist);
             inOrder.verify(roleMapper).toDTOResponse(roleEntity);
+
+        }
+    }
+
+    @Nested
+    @DisplayName("Get pageListRoles Test")
+    class GetPageListRolesTest {
+
+        @Test
+        @DisplayName("Should pageListRoles Success")
+        void testPageListRolesSuccess() {
+            // Arrange
+            int page = 0;
+            int size = 10;
+            Pageable pageable = PageRequest.of(page, size);
+            Page<Role> rolePage = new PageImpl<>(
+                    List.of(roleEntity),
+                    pageable,
+                    1
+            );
+            when(roleServiceDomain.findAll(pageable)).thenReturn(rolePage);
+            when(roleMapper.toDTOResponse(roleEntity)).thenReturn(roleResponse);
+
+            // Act
+            PageResponse<RoleDTOResponse> result = roleServiceImpl.pageListRole(page, size);
+
+            // Assert
+            assertAll(
+                    () -> assertEquals(0,result.page()),
+                    () -> assertEquals(10,result.size()),
+                    () -> assertEquals(1,result.totalElements()),
+                    () -> assertEquals(roleResponse,result.content().get(0))
+            );
+            // InOrder Verify
+            InOrder inOrder = Mockito.inOrder(roleServiceDomain,roleMapper);
+            inOrder.verify(roleServiceDomain).findAll(pageable);
+            inOrder.verify(roleMapper).toDTOResponse(roleEntity);
+
 
         }
     }
