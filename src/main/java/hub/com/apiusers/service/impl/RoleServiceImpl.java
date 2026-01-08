69d38093c678174f9e8f5273b1376a2ba7d0ec42
AlexisFl <159475523+Alexisfllv@users.noreply.github.com>
2026-01-08 16:33:31 -0500

    feat(role): RoleServiceImpl implement method Get pageListRole

--- src/main/java/hub/com/apiusers/service/impl/RoleServiceImpl.java
+++ src/main/java/hub/com/apiusers/service/impl/RoleServiceImpl.java
@@ -6,8 +6,12 @@ import hub.com.apiusers.entity.Role;
 import hub.com.apiusers.mapper.RoleMapper;
 import hub.com.apiusers.service.RoleService;
 import hub.com.apiusers.service.domain.RoleServiceDomain;
+import hub.com.apiusers.util.page.PageResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -23,9 +27,28 @@ public class RoleServiceImpl implements RoleService {
     public RoleDTOResponse findByIdRole(Long id) {
         Role roleExist = roleServiceDomain.roleExists(id);
 
-        log.info("Role exists: " + roleExist);
+        log.debug("Role exists: " + roleExist);
         RoleDTOResponse response = roleMapper.toDTOResponse(roleExist);
 
         return response;
+    }
+
+    @Override
+    public PageResponse<RoleDTOResponse> pageListRole(int page, int size) {
+        log.debug("page/size request : {} , {} ", page , size);
+        Pageable pageable = PageRequest.of(page, size);
+
+        Page<Role> rolePage = roleServiceDomain.findAll(pageable);
+
+        return new PageResponse<>(
+                rolePage.getContent()
+                        .stream()
+                        .map(role -> roleMapper.toDTOResponse(role))
+                        .toList(),
+                rolePage.getNumber(),
+                rolePage.getSize(),
+                rolePage.getTotalElements(),
+                rolePage.getTotalPages()
+        );
     }
 }
