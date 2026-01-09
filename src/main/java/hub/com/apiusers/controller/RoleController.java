60a723ad0e7589094f29fcb01c0e999c8438116f
AlexisFl <159475523+Alexisfllv@users.noreply.github.com>
2026-01-09 14:09:03 -0500

    feat(role): RoleController add Post createRolePost 201

--- src/main/java/hub/com/apiusers/controller/RoleController.java
+++ src/main/java/hub/com/apiusers/controller/RoleController.java
@@ -1,11 +1,13 @@
 package hub.com.apiusers.controller;
 
 
+import hub.com.apiusers.dto.role.RoleDTORequest;
 import hub.com.apiusers.dto.role.RoleDTOResponse;
 import hub.com.apiusers.service.RoleService;
 import hub.com.apiusers.util.ApiResponse.GenericResponse;
 import hub.com.apiusers.util.ApiResponse.StatusApi;
 import hub.com.apiusers.util.page.PageResponse;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -36,5 +38,15 @@ public class RoleController {
         PageResponse<RoleDTOResponse> paged = roleService.pageListRole(page, size);
         return ResponseEntity.status(HttpStatus.OK)
                 .body(new GenericResponse<>(StatusApi.SUCCESS,paged));
+    }
+
+    //POST
+
+    // createRole
+    @PostMapping
+    public ResponseEntity<GenericResponse<RoleDTOResponse>> createRolePost(@Valid @RequestBody RoleDTORequest request){
+        RoleDTOResponse response = roleService.createRole(request);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(new GenericResponse<>(StatusApi.CREATED,response));
     }
 }
