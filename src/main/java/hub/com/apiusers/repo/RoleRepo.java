5d27158757a9927603e95f8c44eaca2f3d8982dc
AlexisFl <159475523+Alexisfllv@users.noreply.github.com>
2026-01-10 09:40:00 -0500

    feat(role): RoleRepo add boolean existsByName()

--- src/main/java/hub/com/apiusers/repo/RoleRepo.java
+++ src/main/java/hub/com/apiusers/repo/RoleRepo.java
@@ -4,4 +4,5 @@ import hub.com.apiusers.entity.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RoleRepo extends JpaRepository<Role,Long> {
+    boolean existsByName(String name);
 }
