75d49c427d9862922021b4c7dbc70ce0032f34e9
AlexisFl <159475523+Alexisfllv@users.noreply.github.com>
2026-01-12 17:06:55 -0500

    feat(user): UserService add interface method pageListUser

--- src/main/java/hub/com/apiusers/service/UserService.java
+++ src/main/java/hub/com/apiusers/service/UserService.java
@@ -1,11 +1,16 @@
 package hub.com.apiusers.service;
 
 import hub.com.apiusers.dto.user.UserDTOResponse;
+import hub.com.apiusers.util.page.PageResponse;
+import org.springframework.data.domain.PageRequest;
 
 public interface UserService {
     // GET
 
     // findByIdUser
     UserDTOResponse findByIdUser(Long id);
+
+    // pageListUser
+    PageResponse<UserDTOResponse> pageListUser(int page, int size);
 
 }
