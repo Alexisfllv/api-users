0d7f95fd89538b29e6f0cf3e4f5bc2a0b2b0cb9f
AlexisFl <159475523+Alexisfllv@users.noreply.github.com>
2026-01-15 22:36:36 -0500

    feat(user): UserRepo add findByActiveTrue

--- src/main/java/hub/com/apiusers/repo/UserRepo.java
+++ src/main/java/hub/com/apiusers/repo/UserRepo.java
@@ -1,9 +1,15 @@
 package hub.com.apiusers.repo;
 
 import hub.com.apiusers.entity.User;
+import hub.com.apiusers.projection.user.UserView;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 public interface UserRepo extends JpaRepository<User,Long> {
     boolean existsByUsername(String username);
     boolean existsByEmail(String email);
+
+    // projection
+    List<UserView> findByActiveTrue();
 }
